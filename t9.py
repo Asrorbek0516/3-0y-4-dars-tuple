3b64d3016332b46f414f6185b891d109b57bd0bc
Asrorbek0516 <asrorbekalimqulov@mail.com>
2026-07-04 11:48:08 +0500

    tuple 19-misol

---
+++ t9.py
@@ -0,0 +1,11 @@
+sonlar = (1,2,3,5,6,7,8,9,34,56,78,12)
+
+q = int(input("qidiruvdagi son: "))
+
+if q in sonlar:
+    print("royxatda bor")
+
+else:
+    print("bu son royxatda yoq")
+
+print(sonlar)
